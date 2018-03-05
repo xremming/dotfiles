27b86e3ba61046f6f3b9468ed59e259ea994f97d
Maximilian Remming <maxremming@gmail.com>
2018-03-05 14:06:48 +0200

    instlall docker-compose to /usr/bin

--- dotfiles.sh
+++ dotfiles.sh
@@ -159,8 +159,8 @@ sudo systemctl start docker
 
 # docker-compose
 docker_compose_version=1.18.0
-sudo curl -L "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/${docker_compose_version}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
 # TODO install rust and alacritty
